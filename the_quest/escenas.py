9c95b4dea53898255144c10718c99cea0923010b
DESKTOP-RG3IC9D\alvar <alvaro82vr@gmail.com>
2021-10-11 18:29:43 +0200

    Hasta pantalla instrucciones

--- the_quest/escenas.py
+++ the_quest/escenas.py
@@ -1,5 +1,6 @@
 import pygame as pg
-from . import FPS
+from pygame.constants import KEYDOWN
+from . import FPS, ANCHO
 
 class Escena():
     def __init__(self, pantalla): # le paso la pantalla donde se va acrear la escena
@@ -11,20 +12,57 @@ class Escena():
 
 class Portada(Escena):
     def __init__(self, pantalla):
-        super().__init__(pantalla)
+        super().__init__(pantalla) # Hereda de class Escena
         self.background = pg.image.load("resources/images/bg_portada.png")
         self.logo = pg.image.load("resources/images/logo.png")
+        fuente = pg.font.Font("resources/fonts/nasalization-rg.otf", 40)
+        self.callToAction = fuente.render("Pulsa <SCP> para continuar", True, (255, 255, 255))
+        self.anchoTexto = self.callToAction.get_width()
 
     def bucle_principal(self):
+        print("soy partida")
         game_over = False
         while not game_over:
             for evento in pg.event.get(): # Recupero eventos
                 if evento.type == pg.QUIT:
-                    game_over = True
-            self.pantalla.fill((19, 41, 81))
+                    exit()
+                
+                if evento.type == pg.KEYDOWN:
+                    if evento.key == pg.K_SPACE:
+                        game_over = True
 
+
+            #self.pantalla.fill((19, 41, 81))
             self.pantalla.blit(self.background, (0,0))
             self.pantalla.blit(self.logo, (230, 250))
+            self.pantalla.blit(self.callToAction, ((ANCHO-self.anchoTexto)//2, 550)) # Obtener tamaño texto y centrado
+
+            pg.display.flip()
+
+
+
+class Instrucciones(Escena):
+    def __init__(self, pantalla):
+        super().__init__(pantalla)
+        self.background = pg.image.load("resources/images/bg_instrucciones.jpg")
+        fuente = pg.font.Font("resources/fonts/nasalization-rg.otf", 60)
+        self.texto = fuente.render("Pulsa <SCP> para JUGAR", True, (255, 255, 255))
+        self.anchoTexto = self.texto.get_width()
+
+    def bucle_principal(self):
+        print("soy instrucciones")
+        game_over = False
+        while not game_over:
+            for evento in pg.event.get():
+                if evento.type == pg.QUIT:
+                    exit()
+
+                if evento.type == pg.KEYDOWN:
+                    if evento.key == pg.K_SPACE:
+                        game_over = True
+
+            self.pantalla.blit(self.background, (0, 0))
+            self.pantalla.blit(self.texto, ((ANCHO-self.anchoTexto)//2 , 650))
 
             pg.display.flip()
 
--- the_quest/game.py
+++ the_quest/game.py
@@ -1,16 +1,22 @@
 import pygame as pg
 from the_quest import ALTO, ANCHO
-from the_quest.escenas import Portada
+from the_quest.escenas import Instrucciones, Portada
 
 pg.init()
 
 class Game():
     def __init__(self):
         pantalla = pg.display.set_mode((ANCHO, ALTO)) # esta pantalla hay que pasarsela a cada una de las escenas
-        self.escenas = [Portada(pantalla)]
+        self.escenas = [Portada(pantalla), Instrucciones(pantalla)]
 
     def launch(self):
-        self.escenas[0].bucle_principal()
-        pg.quit()
+        i = 0
+        while True:
+            self.escenas[i].bucle_principal()
+            i += 1
+            if i == len(self.escenas):
+                i = 0
+    
+        
 
 
