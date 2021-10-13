73217db9ec7ae6cfa23710b32054a0fbf47278ab
DESKTOP-RG3IC9D\alvar <alvaro82vr@gmail.com>
2021-10-14 00:15:54 +0200

    Hasta asteroides random + Sprait Groups

--- the_quest/escenas.py
+++ the_quest/escenas.py
@@ -78,9 +78,6 @@ class Partida(Escena):
         self.background = pg.image.load("resources/images/bg_partida2.jpg").convert()
         self.x = 0 # Para animar background
 
-        #GRUPO
-        self.todos = pg.sprite.Group()
-        
         # NAVE
         self.player = Nave(midleft=(ANCHO-1200, ALTO//2)) # posicionamiento viene del **kwargs (init clase Nave)
 
@@ -90,9 +87,14 @@ class Partida(Escena):
         self.asteroides = pg.sprite.Group()
         for i in range(10):
             asteroide = Asteroide(center=(random.randrange(ANCHO+50, ANCHO+500), random.randrange(40, ALTO-40)))
-            self.todos.add(asteroide)
+            self.asteroides.add(asteroide)
 
-        self.todos.add(self.asteroides, self.player)
+        # GRUPOS
+        self.grupo_player = pg.sprite.Group()
+        self.grupo_asteroides = pg.sprite.Group()
+
+        self.grupo_player.add(self.player)
+        self.grupo_asteroides.add(self.asteroides)
 
 
 
@@ -109,7 +111,8 @@ class Partida(Escena):
             #self.player.update(dt)
             #for i in range(10):
                 #self.asteroides[i].update(dt)
-            self.todos.update(dt)
+            self.grupo_player.update(dt)
+            self.grupo_asteroides.update(dt)
 
             # BLITS
             x_relativa = self.x % self.background.get_rect().width
@@ -122,8 +125,9 @@ class Partida(Escena):
 
             #for asteroide in self.asteroides:
                 #self.pantalla.blit(asteroide.image, asteroide.rect)
-            self.todos.draw(self.pantalla)
-            
+            self.grupo_player.draw(self.pantalla)
+            self.grupo_asteroides.draw(self.pantalla)
+
 
 
             pg.display.flip()
